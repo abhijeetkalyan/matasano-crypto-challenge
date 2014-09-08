f95f93aab2b83412183a8c54e4c7478a03a51d4a
abhijeetkalyan <abhijeetkalyan@gmail.com>
2014-09-08 19:46:22 +0530

    Added challenge 2

---
+++ set-1/c2.rb
@@ -0,0 +1,26 @@
+=begin
+
+Fixed XOR
+
+Write a function that takes two equal-length buffers and produces their XOR combination.
+
+If your function works properly, then when you feed it the string:
+
+1c0111001f010100061a024b53535009181c
+
+... after hex decoding, and when XOR'd against:
+
+686974207468652062756c6c277320657965
+
+... should produce:
+
+746865206b696420646f6e277420706c6179
+
+=end
+
+def fixed_xor(str1, str2)
+	out = str1.hex ^ str2.hex
+	out.to_s(16)
+end
+
+puts fixed_xor('1c0111001f010100061a024b53535009181c', '686974207468652062756c6c277320657965')
